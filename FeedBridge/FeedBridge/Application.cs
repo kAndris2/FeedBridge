fc47590a3184d8542b4ad3d8914ef99e7492b457
kAndris2 <k.andris258@gmail.com>
2026-09-04 20:58:58 +0200

    app base created

---
+++ FeedBridge/FeedBridge/Application.cs
@@ -0,0 +1,14 @@
+﻿using FeedBridge.Services;
+
+namespace FeedBridge
+{
+    public class Application(GoogleDriveService driveService)
+    {
+        private readonly GoogleDriveService _driveService = driveService;
+
+        public void Start()
+        {
+
+        }
+    }
+}
--- FeedBridge/FeedBridge/Program.cs
+++ FeedBridge/FeedBridge/Program.cs
@@ -1,12 +1,15 @@
-﻿using FeedBridge.Services;
+﻿using FeedBridge;
+using FeedBridge.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-builder.Services.AddTransient<GoogleDriveService>();
+builder.Services.AddScoped<GoogleDriveService>();
+builder.Services.AddScoped<Application>();
 
 using var host = builder.Build();
 
-var googleDriveService =
-    host.Services.GetRequiredService<GoogleDriveService>();
+host.Services
+    .GetRequiredService<Application>()
+    .Start();
