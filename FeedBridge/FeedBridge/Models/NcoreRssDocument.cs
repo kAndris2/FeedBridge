393affabc21ffca4ad60a1df39a2bf29c8e4aebe
kAndris2 <k.andris258@gmail.com>
2026-09-04 23:23:19 +0200

    rss document models created

---
+++ FeedBridge/FeedBridge/Models/NcoreRssDocument.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Serialization;
+
+namespace FeedBridge.Models
+{
+    [XmlRoot(ElementName = "item")]
+    public class Item
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+
+        [XmlElement(ElementName = "link")]
+        public string DownloadUrl { get; set; }
+
+        [XmlElement(ElementName = "category")]
+        public string Category { get; set; }
+    }
+
+    [XmlRoot(ElementName = "channel")]
+    public class Channel
+    {
+        [XmlElement(ElementName = "item")]
+        public List<Item> Item { get; set; }
+    }
+
+    [XmlRoot(ElementName = "rss")]
+    public class NcoreRssDocument
+    {
+        [XmlElement(ElementName = "channel")]
+        public Channel Channel { get; set; }
+    }
+}
