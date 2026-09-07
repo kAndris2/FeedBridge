e40ce6f0af36b5e620ed26fe30ed4e2d98875650
kAndris2 <k.andris258@gmail.com>
2026-09-07 19:54:38 +0200

    nlog installed

--- FeedBridge/FeedBridge/Program.cs
+++ FeedBridge/FeedBridge/Program.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using FeedBridge;
-using FeedBridge.Services;
 using FeedBridge.Models.Configuration;
+using FeedBridge.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddNLog();
 
 builder.Services.Configure<GoogleCredentials>(
     builder.Configuration.GetSection("GoogleCredentials"));
