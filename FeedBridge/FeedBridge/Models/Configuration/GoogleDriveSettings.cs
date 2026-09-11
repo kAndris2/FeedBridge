6ce5e3c5426410627e1bc2bfcce8bb257b613e16
kAndris2 <k.andris258@gmail.com>
2026-09-11 23:57:39 +0200

    drive settings updated

--- FeedBridge/FeedBridge/Models/Configuration/GoogleDriveSettings.cs
+++ FeedBridge/FeedBridge/Models/Configuration/GoogleDriveSettings.cs
@@ -3,6 +3,12 @@
     public class GoogleDriveSettings
     {
         public string TargetFolder { get; set; }
-        public string JsonFileName { get; set; }
+
+        private string _jsonFileName;
+        public string JsonFileName 
+        {
+            get => _jsonFileName;
+            set => _jsonFileName = Path.GetFileNameWithoutExtension(value) + ".json";
+        }
     }
 }
