8abb4b6a107fbda7ab388987de3cf396a03785d9
kAndris2 <k.andris258@gmail.com>
2026-09-12 00:41:30 +0200

    google drive service updated, added read json method

--- FeedBridge/FeedBridge/Services/GoogleDriveService.cs
+++ FeedBridge/FeedBridge/Services/GoogleDriveService.cs
@@ -1,46 +1,50 @@
 ﻿using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Drive.v3;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
-using FeedBridge.Models.Configuration;
 using FeedBridge.Models;
+using FeedBridge.Models.Configuration;
 
 namespace FeedBridge.Services
 {
     public class GoogleDriveService
     {
+        private readonly ILogger<GoogleDriveService> _logger;
         private readonly GoogleDriveSettings _driveSettings;
         private readonly DriveService _driveService;
         private readonly StorageInfo _storageInfo;
 
-        public GoogleDriveService(IOptions<GoogleCredentials> credentials, IOptions<GoogleDriveSettings> driveSettings)
+        public GoogleDriveService(IOptions<GoogleCredentials> credentials, IOptions<GoogleDriveSettings> driveSettings, ILogger<GoogleDriveService> logger)
         {
             _driveSettings = driveSettings.Value;
-
-            var clientSecrets = new ClientSecrets
-            {
-                ClientId = credentials.Value.ClientId,
-                ClientSecret = credentials.Value.ClientSecret
-            };
-
-            var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                clientSecrets,
-                [
-                    DriveService.Scope.Drive
-                ],
-                "user",
-                CancellationToken.None,
-                new FileDataStore("GoogleAuth", true)
-            ).Result;
-
-            _driveService = new DriveService(new BaseClientService.Initializer
-            {
-                HttpClientInitializer = credential
-            });
-
+            _logger = logger;
+            _driveService = CreateDriveService(credentials.Value).Result;
             _storageInfo = FetchStorageInfo().Result;
+        }
+
+        public async Task<T[]> ReadJsonAsync<T>()
+        {
+            try
+            {
+                await using var stream = new MemoryStream();
+
+                var request = _driveService.Files.Get(_storageInfo.FileId);
+                await request.DownloadAsync(stream);
+
+                stream.Position = 0;
+
+                return await JsonSerializer.DeserializeAsync<T[]>(stream)
+                    ?? throw new JsonException($"Failed to deserialize storage file '{_storageInfo.FileId}'.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex.Message);
+                return [];
+            }
         }
 
         public async Task UploadJsonAsync(string content)
@@ -58,6 +62,45 @@ namespace FeedBridge.Services
             }
         }
 
+        private async Task<DriveService> CreateDriveService(GoogleCredentials credentials)
+        {
+            return new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = await Authorize(credentials)
+            });
+        }
+
+        private async Task<UserCredential> Authorize(GoogleCredentials credentials)
+        {
+            var clientSecrets = new ClientSecrets
+            {
+                ClientId = credentials.ClientId,
+                ClientSecret = credentials.ClientSecret
+            };
+
+            try
+            {
+                var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    clientSecrets,
+                    [
+                        DriveService.Scope.Drive
+                    ],
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore("GoogleAuth", true)
+                );
+
+                _logger.LogInformation("Successfully authorized to Google!");
+
+                return credential;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An unexpected error occurred during Google authorization! - Ex.: {ex.Message}");
+                throw;
+            }
+        }
+
         private async Task CreateStorageFile(MemoryStream stream)
         {
             var createRequest = _driveService.Files.Create(
@@ -72,6 +115,8 @@ namespace FeedBridge.Services
             createRequest.Fields = "id";
 
             await createRequest.UploadAsync();
+
+            _logger.LogInformation($"Storage file created! - Filename: {_driveSettings.JsonFileName} | Folder: {_driveSettings.TargetFolder}");
         }
 
         private async Task UpdateStorageFile(MemoryStream stream)
@@ -85,12 +130,16 @@ namespace FeedBridge.Services
             updateRequest.Fields = "id";
 
             await updateRequest.UploadAsync();
+
+            _logger.LogInformation($"Storage file updated! - Filename: {_driveSettings.JsonFileName} | Folder: {_driveSettings.TargetFolder}");
         }
 
         private async Task<StorageInfo> FetchStorageInfo()
         {
             var folderId = await FindStorageFolderId();
             var storageFileId = await FindStorageFileId(folderId);
+
+            _logger.LogInformation("Storage information collected!");
 
             return new StorageInfo(folderId, storageFileId);
         }
